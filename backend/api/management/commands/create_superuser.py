ce3f7244a3072fe2fd333ef6057631d3402ac213
Антон Серебряков <antonserebrakov@MacBook-Air-Anton.local>
2024-05-23 01:34:49 +0200

    1st

--- backend/api/management/commands/create_superuser.py
+++ backend/api/management/commands/create_superuser.py
@@ -1,14 +1,15 @@
 import os
 
-from django.core.management.base import BaseCommand
 from api.models import CustomUser
+from django.core.management.base import BaseCommand
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class Command(BaseCommand):
-    help = 'Creates a superuser from environment variables if no superuser exists.'
+    help = 'Creates a superuser from environment'
+    ' variables if no superuser exists.'
 
     def handle(self, *args, **options):
         if not CustomUser.objects.filter(is_superuser=True).exists():
@@ -18,7 +19,7 @@ class Command(BaseCommand):
                 password=os.getenv('DJANGO_SUPERUSER_PASSWORD'),
                 first_name=os.getenv('DJANGO_SUPERUSER_FIRST_NAME'),
                 last_name=os.getenv('DJANGO_SUPERUSER_LAST_NAME'))
-            self.stdout.write(self.style.SUCCESS('Successfully created a new superuser'))
+            self.stdout.write(self.style.SUCCESS('Successfully created'
+                                                 ' a new superuser'))
         else:
             self.stdout.write(self.style.WARNING('A superuser already exists'))
-
--- backend/api/management/commands/import_ingredients.py
+++ backend/api/management/commands/import_ingredients.py
@@ -1,17 +1,19 @@
 
 import json
 
+from api.models import Ingredient
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
-
-from api.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Imports data from a JSON file into the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('json_file', type=str, help='Path to the JSON file to be imported')
+        parser.add_argument('json_file',
+                            type=str,
+                            help='Path to the JSON '
+                            'file to be imported')
 
     def handle(self, *args, **kwargs):
         json_file = kwargs['json_file']
@@ -23,7 +25,9 @@ class Command(BaseCommand):
                     product.full_clean()  # Проверка на валидность данных
                     product.save()
                 except ValidationError as e:
-                    self.stdout.write(self.style.ERROR(f"Error importing item {item}: {e.message_dict}"))
+                    self.stdout.write(self.style.ERROR(f'Error import {item}: '
+                                                       f'{e.message_dict}'))
                 except Exception as e:
-                    self.stdout.write(self.style.ERROR(f"Unexpected error importing item {item}: {str(e)}"))
+                    self.stdout.write(self.style.ERROR(f'Error import {item}'
+                                                       f': {str(e)}'))
         self.stdout.write(self.style.SUCCESS('Data imported successfully'))
